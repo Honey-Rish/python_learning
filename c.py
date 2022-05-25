7f38d9b3fe9dc84ce37337c85487a0fb1938ae1c
CHARITHA <90490913+TOBEKOWN@users.noreply.github.com>
2022-05-25 21:28:08 +0530

    Add files via upload

---
+++ c.py
@@ -0,0 +1,12 @@
+x=1
+x=2
+x=3
+x=4
+if (x==1):
+    print('one')
+elif(x==2):
+    print('two')
+elif(x==3):
+    print('three')
+elif(x==4):
+    print('four')
---
+++ college.py
@@ -0,0 +1,82 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2**3
+8
+print("happy",24,'True')
+happy 24 True
+\
+print("'hello'world")
+'hello'world
+print("happy,24,'True'")
+happy,24,'True'
+print(charitha*100)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print(charitha*100)
+NameError: name 'charitha' is not defined
+print("charitha*20")
+charitha*20
+print(20*"charitha")
+charithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharithacharitha
+x=2
+y=2
+x+y
+4
+x=7
+x=y
+x=y
+x+y
+4
+x=5
+x+y
+7
+print("name = charitha")
+name = charitha
+print(Name = i am a good student)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+a ="i am a good student"
+print(a=[4])a ="i am a good student"
+SyntaxError: invalid syntax
+print(a=[4])
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    print(a=[4])
+TypeError: 'a' is an invalid keyword argument for print()
+print(a[4])
+ 
+print(a[4])
+ 
+
+
+
+
+
+
+charitha="i am a good student"
+print(charitha[4])
+ 
+print(charitha[-1])
+t
+print(charitha[7;])
+SyntaxError: invalid syntax
+print(charitha[7:11])
+good
+cherry="i am good girl student"
+print(charitha+cherry)
+i am a good studenti am good girl student
+name ='cherry'
+print(len[name])
+Traceback (most recent call last):
+  File "<pyshell#37>", line 1, in <module>
+    print(len[name])
+TypeError: 'builtin_function_or_method' object is not subscriptable
+print(len(name))
+6
+type(name)
+<class 'str'>
+type(x)
+<class 'int'>
+1=['manu',24]
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+name=['manu, lets enjoy']
+name.append('hi')
